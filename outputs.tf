f492c8fb231beccfa67303e98da37c2d8a3f8b13
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-07-08 13:17:52 +0530

    outputs

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "api_endpoint" {
   value = module.api_gateway.api_endpoint
 }
 
-output "ses_identity_verification_status" {
-  description = "Email verification status in SES"
-  value       = module.ses.ses_identity_verification_status
-}
+#output "ses_identity_verification_status" {
+#  description = "Email verification status in SES"
+#  value       = module.ses.ses_identity_verification_status
+#}
