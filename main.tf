babcda3bef22ff2c2380409e66ffa12c2ab57a76
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-07-08 12:58:14 +0530

    added modules

--- main.tf
+++ main.tf
@@ -8,6 +8,17 @@ module "lambda" {
   source              = "./modules/lambda"
   lambda_zip     = data.archive_file.lambda_zip.output_path
   lambda_name         = var.lambda_function_name
+  aws_dynamodb_table_arn = module.dynamodb.aws_dynamodb_table_arn
+}
+
+module "dynamodb" {
+  source             = "./modules/dynamodb"
+}
+
+module "ses" {
+  source             = "./modules/ses"
+  email_recipient    = var.email_recipient
+
 }
 
 module "api_gateway" {
